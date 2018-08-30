1b4ff5db69a916ae199b378a188407dbe54d59f9
linjiaxing <linjiaxing@zhihu.com>
2018-08-30 13:02:13 +0800

    update

--- ip-test/ip_test.py
+++ ip-test/ip_test.py
@@ -3,12 +3,26 @@ from air import iplib
 
 class IpTest:
 
-    def find(self):
-        return iplib.find("172.217.24.35")
+    def find(self, ip):
+        return iplib.find(ip)
 
 
 
 
 test = IpTest()
 
-print test.find()
+# print test.find("172.217.24.35")
+# print test.find("127.0.0.1")
+# print test.find("192.168.1.100")
+
+# tmp = test.find("10.13.63.255")
+# print tmp["country_code"] != '*'
+
+print test.find("172.217.24.35")
+#print test.find("61.135.169.125")
+# print test.find("123.125.104.197")
+# print test.find("111.111.111.111")
+# print test.find("222.222.222.222")
+# print test.find("221.222.222.222")
+# print test.find("220.222.222.222")
+# print test.find("219.222.222.222")
---
+++ sc.py
@@ -0,0 +1,6 @@
+
+sku = [1, 2, 3, 4, 5, 6]
+
+ids = [id == 5 for id in sku]
+
+print id
