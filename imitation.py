3563fdd9ec4eb93ed411dd02b5b3452687090c6f
linjiaxing <linjiaxing@zhihu.com>
2018-11-10 15:51:20 +0800

    fix

---
+++ imitation.py
@@ -0,0 +1,10 @@
+1 order_type = 'special_gift' if item.sku_kind == SKUKind.SKU_KIND_PACKAGE else 'gift'
+
+2 "100" if config.fee_payer == "zhihu" else "0"
+
+3 result = [val for val in DEMO_SKU_CONFIG if val.sku_id == str(sku_id)]
+  return result[0] if result else None
+
+4 sum([val.real_amount for val in items])
+
+
